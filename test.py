b7a087a9c5f5c0b86e276df000b5ca452a45c424
haiphong93 <47393412+haiphong93@users.noreply.github.com>
2019-06-17 17:18:57 +0200

    Update test.py

--- test.py
+++ test.py
@@ -118,14 +118,5 @@ model.compile(loss='binary_crossentropy', optimizer=adam, metrics = [roc_auc])
 model.load_weights('best_model.hdf5')
 model_ouput = model.predict(X_test)
 score = roc_auc_score(y_test, model_ouput)
-print('ROC AUC for raw neural network probability:', score)
-'''
-#Convert neural network output to 0 and 1 label.
-I noticed that having the threshold for the conversion at 0.45 gives better 
-score than having at the traditional 0.5 across a few models I have trained.
-However, I will still leave the threshold at 0.5 for now. 
-'''
-label = (model_ouput>0.5).astype(int)
-score = roc_auc_score(y_test, label)
-print('ROC AUC for 0-1 label conversion output:',score)
+print('ROC AUC score:', score)
 
